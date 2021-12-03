b94d95cd6446c628b496770a72e9d8a701484c6c
JC-A01704730 <91692137+JCiturria@users.noreply.github.com>
2021-12-03 00:31:35 -0600

    Update MainCocina.cpp

--- MainCocina.cpp
+++ MainCocina.cpp
@@ -16,17 +16,21 @@ void menu(){
        << "4. Hacer Galleta:"<<"\n"
        << "5. Hacer Pan:"<<"\n"
        << "6. Mostrar Orden:"<<"\n"
-       << "7. Final:"<<endl;
+       << "7. Terminar:"<<endl;
 }
 
 int main(){
-  int x;
-  int temp_huevos=0,temp_pisos=0,id=0;
+  cout<<"Juan Carlos Iturria A01704730"<<endl;
+  cout<<"Panaderia de Objetos"<<endl;
+  cout<<"Bienvenido a la panaderia!!! Utiliza este programa para crear una orden"
+  " y asignar productos a ella"<<endl;
+  int temp_huevos=0,temp_pisos=0,id=0,id0;
   float temp_harina=0.0,temp_azucar=0.0,temp_levadura=0.0,temp_mantequilla=0.0,
-  temp_molde=0.0,temp_leche=0.0;
+  temp_molde=0.0,temp_leche=0.0,temp_precio=0.0;
   string temp_sabor="",temp_betun="",temp_tipo="",temp_figura="",temp_nombre="";
   int opcion=0;
-  Orden orden1(temp_nombre,0,0,0,0,0,0.0);
+  //se crea una orden vacia para que funcione el prgrama
+  Orden orden1("No hay orden",0,0,0,0,0,0.0);
   while(opcion<7&&opcion>-1){
     menu();
     cin >>opcion;
@@ -37,9 +41,9 @@ int main(){
         cout<<"El numero de la orden es: "<<"\n";
         orden1.numero_orden(id);cout<<"\n";
         cout<<"Tu precio es: "<<endl;
-        cout<<"precio no implementado todavia"<<endl;
-        Orden orden1(temp_nombre,0,0,0,0,0,0.0);
-        break;}
+        cout<<orden1.precio_total()<<endl;
+        orden1.set_nombre(temp_nombre);
+      break;}
       case 2:{
         cout << "Numero de huevos: ";
         cin >>temp_huevos;
@@ -57,8 +61,10 @@ int main(){
         cin >>temp_molde;
         cout << "Numero de pisos: ";
         cin >>temp_pisos;
+        temp_precio=(temp_huevos*3+temp_harina*0.1+temp_azucar*1.5+temp_levadura*0.12+temp_mantequilla*0.1295+
+          temp_molde*10+temp_pisos*1000);
         orden1.agregar_pastelboda(temp_huevos,temp_harina,temp_sabor,temp_azucar,
-        temp_levadura,temp_mantequilla,temp_molde,temp_pisos);
+          temp_levadura,temp_mantequilla,temp_molde,temp_precio,temp_pisos);
       break;}
       case 3:{
         cout << "Numero de huevos: ";
@@ -77,8 +83,10 @@ int main(){
         cin >>temp_molde;
         cout << "Tipo de betun: ";
         cin >>temp_betun;
+        temp_precio=(temp_huevos*3+temp_harina*0.1+temp_azucar*1.5+temp_levadura*0.12+temp_mantequilla*0.1295+
+          temp_molde*10);
         orden1.agregar_cupcake(temp_huevos,temp_harina,temp_sabor,temp_azucar,
-        temp_levadura,temp_mantequilla,temp_molde,temp_betun);
+        temp_levadura,temp_mantequilla,temp_molde,temp_precio,temp_betun);
       break;}
       case 4:{
         cout << "Numero de huevos: ";
@@ -93,31 +101,38 @@ int main(){
         cin >>temp_azucar;
         cout << "Cantidad de mantequilla: ";
         cin >>temp_mantequilla;
+        temp_precio=(temp_huevos*3+temp_harina*0.1+temp_azucar*1.5+temp_mantequilla*0.1295);
         orden1.agregar_galleta(temp_huevos,temp_harina,temp_sabor,temp_figura,
-        temp_azucar,temp_mantequilla);
+        temp_azucar,temp_mantequilla,temp_precio);
       break;}
       case 5:{
-        cout << "Tipo de pan: ";
-        cin >>temp_tipo;
-        cout << "Numero de huevos: ";
-        cin >>temp_huevos;
-        cout << "Cantidad de harina: ";
-        cin >>temp_harina;
-        cout << "Cantidad levadura: ";
-        cin >>temp_levadura;
-        cout << "Cantidad leche: ";
-        cin >>temp_leche;
-        cout << "Cantidad de azucar: ";
-        cin >>temp_azucar;
-        cout << "Cantidad de mantequilla: ";
-        cin >>temp_mantequilla;
-        orden1.agregar_pan(temp_tipo,temp_huevos,temp_harina,temp_levadura,temp_leche,
-        temp_azucar,temp_mantequilla);
+        cout<<"Tipo de pan: ";
+        cin>>temp_tipo;
+        cout<<"Numero de huevos: ";
+        cin>>temp_huevos;
+        cout<<"Cantidad de harina: ";
+        cin>>temp_harina;
+        cout<<"Cantidad de levadura: ";
+        cin>>temp_levadura;
+        cout<<"Cantidad de leche: ";
+        cin>>temp_leche;
+        cout<<"Cantidad de azucar: ";
+        cin>>temp_azucar;
+        cout<<"Cantidad de mantequilla: ";
+        cin>>temp_mantequilla;
+        temp_precio=(temp_huevos*3+temp_harina*0.1+temp_levadura*0.12+temp_leche*0.5+temp_azucar*1.5+temp_mantequilla*0.1295);
+        orden1.agregar_pan(temp_tipo,temp_huevos,temp_harina,temp_levadura,temp_leche,temp_azucar,temp_mantequilla,temp_precio);
       break;}
       case 6:{
-        cout<<"falta implementar"<<endl;
+        cout<<"El nombre de la orden es: "<<orden1.get_nombre()<<endl;
+        orden1.muestra_pastelb();
+        orden1.muestra_cupcake();
+        orden1.muestra_galleta();
+        orden1.muestra_pan();
+        cout << "Tu precio es: " <<orden1.precio_total()<<endl;
       break;}
         }
       }
   return 0;
 }
+
